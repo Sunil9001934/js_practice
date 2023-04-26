ad6850ab9cce7357271f94ccac851ec8eb7abfd1
Sunil9001934 <108231917+Sunil9001934@users.noreply.github.com>
2023-04-26 14:38:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+  const markwight = 95;
+const markHight = 1.95
+const johnWight = 70
+const johnHight = 1.20
+const BmiMark = markHight /markwight **2;
+const BmiJohn = johnWight /(johnHight * johnHight);
+const BmiMarkhigher = BmiMark > BmiJohn;
+console.log(BmiJohn,BmiMark,BmiMarkhigher);
+console.log(BmiMark,BmiJohn)
+
+
+////////////// my first javascript assingment is completed
+/////////////my second challange is start
+if(BmiMark,BmiJohn) {
+  console.log(`mark bmi(${BmiJohn})is higher than mark(${BmiMark})`)
+
+}else{
+  console.log(`john Bmi (${BmiMark})is higher than john(${BmiJohn})this is a great code and grat challnge this coding opprt`)
+}
